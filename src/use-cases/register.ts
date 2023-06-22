0005b413acf306647e426ca550a502a5cb516f9f
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-21 21:26:58 -0300

    validate error

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-alredy-exists-error'
 
 interface IPropsRegisterUseCaseRequest {
   name: string
@@ -17,7 +18,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
