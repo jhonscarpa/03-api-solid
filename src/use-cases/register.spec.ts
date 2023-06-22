8bbde927214d5e6bb0b2a9e1d0d949492555cdd7
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-22 19:48:49 -0300

    create test should be able to register

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -5,6 +5,18 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { UserAlreadyExistsError } from './errors/user-alredy-exists-error'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const UsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(UsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
   it('should hash user password upon registration', async () => {
     const UsersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(UsersRepository)
