43c5a3da65cd7002f310572002ceba7a031aae1e
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-22 19:41:37 -0300

    create in memory for test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,12 @@
 import { it, describe, expect } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null
-      },
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          created_at: new Date(),
-          email: data.email,
-          password_hash: data.password_hash,
-        }
-      },
-    })
+    const UsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(UsersRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
@@ -32,4 +21,21 @@ describe('Register Use Case', () => {
 
     expect(isPasswordCorrectlyHashed).toBe(true)
   })
+  // it('should hash user password upon registration', async () => {
+  //   const UsersRepository = new InMemoryUsersRepository()
+  //   const registerUseCase = new RegisterUseCase(UsersRepository)
+
+  //   const { user } = await registerUseCase.execute({
+  //     name: 'John Doe',
+  //     email: 'johndoe@example.com',
+  //     password: '123456',
+  //   })
+
+  //   const isPasswordCorrectlyHashed = await compare(
+  //     '123456',
+  //     user.password_hash,
+  //   )
+
+  //   expect(isPasswordCorrectlyHashed).toBe(true)
+  // })
 })
