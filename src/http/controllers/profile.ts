ebb6aee2627daa0f7bf736b1b2a9e0c255e3f6f4
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-06-29 19:35:43 -0300

    validate jwt and create middleware verify token

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,17 @@
+import { MakeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  const getUserProfile = MakeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
