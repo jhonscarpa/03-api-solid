70e9ede6780214507d77bb0260265f1d4516b6b0
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-07-01 11:00:07 -0300

    create router checkIns history

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,21 @@
+import { MakeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const checkInHistoryQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page } = checkInHistoryQuerySchema.parse(request.query)
+
+  const fetchUserCheckInsHistoryUsePage = MakeFetchUserCheckInsHistoryUseCase()
+  const { checkIns } = await fetchUserCheckInsHistoryUsePage.execute({
+    userId: request.user.sub,
+    page,
+  })
+
+  return reply.status(200).send({
+    checkIns,
+  })
+}
